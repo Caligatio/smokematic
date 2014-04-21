fd7e27befaeb851748ca1c639677ee9bbd8e54f8
Brian Turek <brian.turek@gmail.com>
2014-04-21 16:32:14 -0400

    Cleaned up the setup.py and added classifiers

--- setup.py
+++ setup.py
@@ -1,24 +1,32 @@
 from setuptools import setup
 
 setup(
-    name = "smokematic",
-    version = "0.1.0",
-    author = "Brian Turek",
-    author_email = "brian.turek@gmail.com",
-    description = "A smoking automation system using the Beaglebon Black",
-    license = "BSD",
-    url = "https://github.com/Caligatio/smokematic",
+    name = 'smokematic',
+    version = '0.1.0',
+    author = 'Brian Turek',
+    author_email = 'brian.turek@gmail.com',
+    description = 'A smoking (cooking) automation system built on the Beaglebone Black',
+    license = 'BSD',
+    url = 'https://github.com/Caligatio/smokematic',
     packages=['smokematic'],
     entry_points = {
-        "console_scripts": [
-            "smokematic = smokematic:entry"
+        'console_scripts': [
+            'smokematic = smokematic:entry'
         ]
     },
-
     install_requires = [
-        "tornado",
-        "Adafruit_BBIO",
-        "jsonschema"
+        'tornado',
+        'Adafruit_BBIO',
+        'jsonschema'
     ],
-    include_package_data=True
+    include_package_data=True,
+    classifiers= [
+        'License :: OSI Approved :: BSD License',
+        'Development Status :: 4 - Beta',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: JavaScript',
+        'Topic :: Home Automation',
+        'Topic :: Software Development :: Embedded Systems'
+    ]
 )
